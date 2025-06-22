53c17974693481bfbb038f8a10d0da944e088a15
Mohammed-Hashir <smdhashir2006@gmail.com>
2025-06-22 21:07:29 +0530

    Update train_spam_model.py

--- train_spam_model.py
+++ train_spam_model.py
@@ -7,31 +7,31 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 import pickle
 
-# 1. Load dataset
+# Loading dataset
 df = pd.read_csv("spam.csv", encoding='latin-1')[['label', 'message']]
 df.columns = ['label', 'message']
 
-# 2. Encode labels: spam = 1, ham = 0
+# Encoding labels: spam = 1, ham = 0
 df['label'] = df['label'].map({'spam': 1, 'ham': 0})
 
-# 3. Split
+# Splitting
 X_train, X_test, y_train, y_test = train_test_split(df['message'], df['label'], test_size=0.2, random_state=42)
 
-# 4. Vectorize text
+# Vectorizing text
 vectorizer = TfidfVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# 5. Train model
+# Training model
 model = MultinomialNB()
 model.fit(X_train_vec, y_train)
 
-# 6. Test
+# Testing
 y_pred = model.predict(X_test_vec)
 acc = accuracy_score(y_test, y_pred)
 print(f"Model Accuracy: {acc:.2f}")
 
-# 7. Save model and vectorizer
+# Saving the model and vectorizer
 with open("spam_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
